5cd114341e81de67ccb61bb6ef8a5764ceb120c4
Blake Sager <bsager@atlassian.com>
2021-08-21 13:26:49 -0500

    add gravity and player info, implement basic play

--- chapter_3/flappy/src/main.rs
+++ chapter_3/flappy/src/main.rs
@@ -6,7 +6,13 @@ enum GameMode {
     End,
 }
 
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
 struct State{
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 
@@ -14,13 +20,29 @@ impl State {
     fn new() -> Self {
         // returns State struct with mode set to menu
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // TODO: implement fn
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -54,10 +76,11 @@ impl State {
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 }
-
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
@@ -66,6 +89,48 @@ impl GameState for State {
             GameMode::End => self.dead(ctx),
             GameMode::Playing=> self.play(ctx,)
         }
+    }
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        )
+    }
+
+    fn gravity_and_move(&mut self) {
+        // apply gravity if terminal velocity has not been reached
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0
     }
 }
 
