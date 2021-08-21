876192d1af8990aeb6af870f320503c9e3cb5ed2
Blake Sager <bsager@atlassian.com>
2021-08-21 08:45:37 -0500

    Finish chap 2

--- chapter_2/treehouse/src/main.rs
+++ chapter_2/treehouse/src/main.rs
@@ -1,31 +1,83 @@
 use std::io::stdin;
 
+#[derive(Debug)]
+struct Visitor {
+    name: String,
+    action: VisitorAction,
+    age: i8,
+}
+
+#[derive(Debug)]
+enum VisitorAction {
+    Accept,
+    AcceptWithNote { note: String },
+    Refuse,
+    Probation,
+}
+
+impl Visitor {
+    // associated function, has to be access from the struct itself
+    // ie: Visitor::new
+    fn new(name: &str, action: VisitorAction, age: i8) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            action,
+            age,
+        }
+    }
+
+    fn greet_visitor(&self) {
+        match &self.action {
+            VisitorAction::Accept => println!("Welcome to the treehouse, {}", self.name),
+            VisitorAction::AcceptWithNote { note } => {
+               println!("Welcome to the treehouse, {}", self.name);
+               println!("{}", note);
+               if self.age < 21 {
+                   println!("Do not serve alcohol to {}", self.name)
+               }
+            }
+            VisitorAction::Probation => println!("{} is now a probationary member", self.name),
+            VisitorAction::Refuse => println!("Do not allow {} in!", self.name)
+        }
+    }
+}
+
 fn what_is_your_name() -> String {
     let mut your_name = String::new();
     stdin()
         .read_line(&mut your_name)
         .expect("Failed to read line");
 
-    your_name
-        .trim()
-        .to_lowercase()
+    your_name.trim().to_lowercase()
 }
 
 fn main() {
-    println!("Hello, what's your name?");
-    let name = what_is_your_name();
+    let mut visitor_list = vec![
+        Visitor::new("bert", VisitorAction::Accept, 45),
+        Visitor::new("steve", VisitorAction::AcceptWithNote{
+            note:String::from("Lactose-free milk is in the fridge")
+        }, 15),
+        Visitor::new("fred", VisitorAction::Refuse, 30),
+    ];
 
-    let mut allow_access = false;
-    let visitor_list = ["bert", "steve", "fred"];
-    for visitor in &visitor_list {
-        if visitor == &name {
-            allow_access = true;
+    loop {
+        println!("Hello, what's your name? (Leave empty and press ENTER to quit)");
+        let name = what_is_your_name();
+
+        let known_visitor = visitor_list.iter().find(|visitor| visitor.name == name);
+
+        match known_visitor {
+            Some(visitor) => visitor.greet_visitor(),
+            None => {
+                if name.is_empty() {
+                    println!("The final list of visitors:");
+                    println!("{:#?}:", visitor_list);
+                    break;
+                } else {
+                    println!("{} is not on the visitor list.", name);
+                    visitor_list.push(Visitor::new(&name, VisitorAction::Probation, 0));
+                }
+            }
         }
-    }
-
-    if allow_access {
-        println!("Weclome to the Treehouse, {}", name);
-    } else {
-        println!("Sorry, you aren't on the list");
     }
 }
